be3efa91c4fb35fac8a399d26efe12036450c840
Quentin Conner <quentin.conner@gmail.com>
2025-04-01 15:20:51 -0500

    back to real aws s3 upload

--- .github/actions/deploy-s3-javascript/s3upload.js
+++ .github/actions/deploy-s3-javascript/s3upload.js
@@ -24,7 +24,7 @@ function run() {
     // github.getOctokit().
     // useful for github API use
 
-    exec.exec(`echo aws s3 sync ${folder} ${s3URI} --region ${region}`);
+    exec.exec(`aws s3 sync ${folder} ${s3URI} --region ${region}`);
 
     const websiteURL = `http://${bucket}.s3-website-${region}.amazonaws.com`;
     core.setOutput('website-url', websiteURL);
